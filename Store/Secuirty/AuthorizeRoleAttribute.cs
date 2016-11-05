eee5fd6e3db5cc50465e25047bcd888da1c09c61
avipunes <pones1@gmail.com>
2016-11-05 18:11:18 +0200

    Add project files.

---
+++ Store/Secuirty/AuthorizeRoleAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Store.Models.DB;
+using Store.Models.EntityManager;
+
+namespace Store.Security
+{
+    public class AuthorizeRolesAttribute : AuthorizeAttribute
+    {
+        private readonly string[] userAssignedRoles;
+        public AuthorizeRolesAttribute(params string[] roles)
+        {
+            this.userAssignedRoles = roles;
+        }
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorize = false;
+            using (mydbEntities db = new mydbEntities())
+            {
+                UserManager UM = new UserManager();
+                foreach (var roles in userAssignedRoles)
+                {
+                    authorize = UM.IsUserInRole(httpContext.User.Identity.Name, roles);
+                    if (authorize)
+                        return authorize;
+                }
+            }
+            return authorize;
+        }
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Home/UnAuthorized");
+        }
+    }
+}
